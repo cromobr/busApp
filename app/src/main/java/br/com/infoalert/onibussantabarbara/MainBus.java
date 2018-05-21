c4bd412840479d386986ddde8c6378c336585a8d
Bruno Elias Cromo <bcromo@cit>
2018-05-21 11:51:27 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/infoalert/onibussantabarbara/MainBus.java
@@ -0,0 +1,13 @@
+package br.com.infoalert.onibussantabarbara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bus);
+    }
+}
---
+++ app/src/main/java/br/com/infoalert/onibussantabarbara/linhaOnibus/domain/itinerario.java
@@ -0,0 +1,9 @@
+package br.com.infoalert.onibussantabarbara.linhaOnibus.domain;
+
+import java.sql.Time;
+
+public class itinerario {
+    private Integer numero;
+    private Time horaSaida;
+
+}
